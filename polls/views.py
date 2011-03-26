5424dcbdbcc31f36c8e5670235df6494a4ad143c
Genevieve Ruebel <ruebelg@gmail.com>
2011-03-26 13:50:48 -0500

    views added

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
+from django.http import HttpResponse
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % (poll_id,))
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % (poll_id,))
+
+def vote(request, poll_id):
+    return HttpResponse("<strong></strong> You're voting on poll %s." % (poll_id,))
